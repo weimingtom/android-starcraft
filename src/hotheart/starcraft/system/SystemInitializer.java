94725c971ab30fd591d43f98c3be942eb7690fc0
korshakov.stepan <korshakov.stepan@7feee052-178f-11de-babb-ddc7aea8d8a1>
2009-05-03 15:18:16 +0000

    Changed startup units

--- src/hotheart/starcraft/system/SystemInitializer.java
+++ src/hotheart/starcraft/system/SystemInitializer.java
@@ -102,24 +102,24 @@ public class SystemInitializer {
 			showMessage("Creating units");
 
 			ObjectPool.addUnit(Unit.getUnit(0, TeamColors.COLOR_RED),
-						66 * 32, 66 * 32);
+						62 * 32, 66 * 32);
 			
 			ObjectPool.addUnit(Unit.getUnit(1, TeamColors.COLOR_RED),
-					66 * 32, 66 * 32);
+					62 * 32, 66 * 32);
 			
-			ObjectPool.addUnit(Unit.getUnit(3, TeamColors.COLOR_RED),
-					66 * 32, 66 * 32);
+//			ObjectPool.addUnit(Unit.getUnit(3, TeamColors.COLOR_RED),
+//					62 * 32, 66 * 32);
 			
 			//Zergs
 			
 			ObjectPool.addUnit(Unit.getUnit(38, TeamColors.COLOR_GREEN),
-					66 * 32, 66 * 32);
+					66 * 32, 62 * 32);
 			
 			ObjectPool.addUnit(Unit.getUnit(37, TeamColors.COLOR_GREEN),
-					66 * 32, 66 * 32);
+					66 * 32, 62 * 32);
 			
-			ObjectPool.addUnit(Unit.getUnit(40, TeamColors.COLOR_GREEN),
-					66 * 32, 66 * 32);
+//			ObjectPool.addUnit(Unit.getUnit(39, TeamColors.COLOR_GREEN),
+//					66 * 32, 62 * 32);
 			
 			//Protoss
 			
@@ -127,11 +127,11 @@ public class SystemInitializer {
 			ObjectPool.addUnit(Unit.getUnit(66, TeamColors.COLOR_BLUE),
 					66 * 32, 66 * 32);
 			
-			ObjectPool.addUnit(Unit.getUnit(68, TeamColors.COLOR_BLUE),
-					66 * 32, 66 * 32);
-			
 			ObjectPool.addUnit(Unit.getUnit(61, TeamColors.COLOR_BLUE),
-					66 * 32, 66 * 32);
+					66 * 32, 6 * 32);
+			
+//			ObjectPool.addUnit(Unit.getUnit(68, TeamColors.COLOR_BLUE),
+//			66 * 32, 66 * 32);
 
 			
 			showMessage("Loading map");

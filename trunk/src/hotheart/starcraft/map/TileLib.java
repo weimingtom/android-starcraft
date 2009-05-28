260a29fe1a3a41822665e86c072c90e1c323611b
korshakov.stepan <korshakov.stepan@7feee052-178f-11de-babb-ddc7aea8d8a1>
2009-05-28 17:05:12 +0000

    Added XML layout for main screen

--- trunk/src/hotheart/starcraft/map/TileLib.java
+++ trunk/src/hotheart/starcraft/map/TileLib.java
@@ -20,12 +20,15 @@ import hotheart.starcraft.utils.FileSystemUtils;
 public class TileLib {
 	public static int[] palette;// Palette - generated from WPE file
 	public static byte[] CV5; // Descriptions of sprites
-	//public static byte[] VR4; // Tiles(8x8)
+	public static byte[] VR4; // Tiles(8x8)
 	public static int[] VX4Indexes; // Mega-tiles(32x32)
-	public static Bitmap[] miniTiles; // Tiles(8x8)
+
+	// public static Bitmap[] miniTiles; // Tiles(8x8)
+
+	// public static int[][] miniTiles;
 
 	public static final void init(int type) {
-		
+
 		String filePrefix = "";
 
 		// This ids used in scenario.chk
@@ -62,31 +65,31 @@ public class TileLib {
 		palette = new int[256];
 
 		for (int i = 0; i < pal.length / 4; i++) {
-			palette[i] = (255 << 24) + ((pal[i * 4]&0xFF)<<16) + ((pal[i * 4 + 1]&0xFF) << 8)
-					+ (pal[i * 4 + 2]&0xFF);
+			palette[i] = (255 << 24) + ((pal[i * 4] & 0xFF) << 16)
+					+ ((pal[i * 4 + 1] & 0xFF) << 8) + (pal[i * 4 + 2] & 0xFF);
 		}
 
 		// Loading tiles files
 
 		CV5 = FileSystemUtils.readAllBytes(filePrefix + ".cv5");
+		VR4 = FileSystemUtils.readAllBytes(filePrefix + ".vr4");
+
+		// miniTiles = new Bitmap[VR4.length / 64];
+		// int[] tmpBuf = new int[64];
+
+		// for (int id = 0; id < miniTiles.length; id++) {
+		//			
+		// int offset = id * 64;
+		//
+		// for (int i = 0; i < 64; i++)
+		// miniTiles[id][i] = palette[VR4[offset + i] & 0xFF];
+		//
+		// //miniTiles[id] = Bitmap.createBitmap(tmpBuf, 8, 8, Config.RGB_565);
+		// }
+
 		byte[] VX4 = FileSystemUtils.readAllBytes(filePrefix + ".vx4");
-		byte[] VR4 = FileSystemUtils.readAllBytes(filePrefix + ".vr4");
-
-		miniTiles = new Bitmap[VR4.length / 64];
-		int[] tmpBuf = new int[64];
-
-		for (int id = 0; id < miniTiles.length; id++) {
-			int offset = id * 64;
-
-			for (int i = 0; i < 64; i++)
-				tmpBuf[i] = palette[VR4[offset + i] & 0xFF];
-
-			miniTiles[id] = Bitmap.createBitmap(tmpBuf, 8, 8, Config.RGB_565);
-		}
-		
-		VX4Indexes = new int[VX4.length/2];
-		for(int i = 0; i< VX4Indexes.length; i++)
-		{
+		VX4Indexes = new int[VX4.length / 2];
+		for (int i = 0; i < VX4Indexes.length; i++) {
 			VX4Indexes[i] = (VX4[i * 2] & 0xFF)
 					+ ((VX4[i * 2 + 1] & 0xFF) << 8);
 		}
@@ -108,44 +111,51 @@ public class TileLib {
 	}
 
 	public static final void drawMegaTile(int x, int y, int vx4Id, Canvas c) {
-		int index = vx4Id*16;
-		
-		drawTile(x, 	 y, VX4Indexes[index++], c);
-		drawTile(x + 8,  y, VX4Indexes[index++], c);
+		int index = vx4Id * 16;
+
+		drawTile(x, y, VX4Indexes[index++], c);
+		drawTile(x + 8, y, VX4Indexes[index++], c);
 		drawTile(x + 16, y, VX4Indexes[index++], c);
 		drawTile(x + 24, y, VX4Indexes[index++], c);
-		
-		drawTile(x, 	 y + 8, VX4Indexes[index++], c);
-		drawTile(x + 8,  y + 8, VX4Indexes[index++], c);
+
+		drawTile(x, y + 8, VX4Indexes[index++], c);
+		drawTile(x + 8, y + 8, VX4Indexes[index++], c);
 		drawTile(x + 16, y + 8, VX4Indexes[index++], c);
 		drawTile(x + 24, y + 8, VX4Indexes[index++], c);
-		
-		drawTile(x, 	 y + 16, VX4Indexes[index++], c);
-		drawTile(x + 8,  y + 16, VX4Indexes[index++], c);
+
+		drawTile(x, y + 16, VX4Indexes[index++], c);
+		drawTile(x + 8, y + 16, VX4Indexes[index++], c);
 		drawTile(x + 16, y + 16, VX4Indexes[index++], c);
 		drawTile(x + 24, y + 16, VX4Indexes[index++], c);
-		
-		
-		drawTile(x, 	 y + 24, VX4Indexes[index++], c);
-		drawTile(x + 8,  y + 24, VX4Indexes[index++], c);
+
+		drawTile(x, y + 24, VX4Indexes[index++], c);
+		drawTile(x + 8, y + 24, VX4Indexes[index++], c);
 		drawTile(x + 16, y + 24, VX4Indexes[index++], c);
 		drawTile(x + 24, y + 24, VX4Indexes[index++], c);
 	}
+
+	private static final int[] tmpBuf = new int[64];
 
 	public static final void drawTile(int x, int y, int id, Canvas c) {
 		Paint p = new Paint();
 		boolean flipped = (id & 1) == 1;
 		id = id >> 1;
 		Matrix transform = new Matrix();
-		if (!flipped)
-		{
+		if (!flipped) {
 			transform.postTranslate(x, y);
-		}
-		else
-		{
+		} else {
 			transform.preScale(-1, 1);
-			transform.postTranslate(x+8, y);
+			transform.postTranslate(x + 8, y);
 		}
-		c.drawBitmap(miniTiles[id], transform, p);
+		int offset = id * 64;
+		for (int i = 0; i < 64; i++)
+			tmpBuf[i] = palette[VR4[offset + i] & 0xFF];
+
+		c.drawBitmap(tmpBuf, 0, 8, 0, 0, 8, 8, false, p);
+		
+		// miniTiles[id] = Bitmap.createBitmap(tmpBuf, 8, 8,
+		// Config.RGB_565);
+		
+		// c.drawBitmap(miniTiles[id], transform, p);
 	}
 }

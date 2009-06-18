668db8b743d076dfac2f189492a7aef8c97cf580
korshakov.stepan <korshakov.stepan@7feee052-178f-11de-babb-ddc7aea8d8a1>
2009-06-18 23:37:11 +0000

    OpenGL render update - drawing 'images' on there places. Test version

--- src/hotheart/starcraft/graphics/render/opengl/OpenGLRenderImage.java
+++ src/hotheart/starcraft/graphics/render/opengl/OpenGLRenderImage.java
@@ -1,17 +1,55 @@
 package hotheart.starcraft.graphics.render.opengl;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+
+import javax.microedition.khronos.opengles.GL10;
+
+import android.graphics.Color;
+
 import hotheart.starcraft.graphics.render.RenderImage;
 
 public class OpenGLRenderImage extends RenderImage {
 
+	static IntBuffer mVertexBuffer;
+	static IntBuffer mColorBuffer;
+	static ByteBuffer mIndexBuffer;
+	
+	static int[] coords = { 
+			10, 10,
+			10, 0,
+			0, 0,
+			0, 10};
+
+	static int[] colors = { Color.RED, Color.BLUE, Color.WHITE, Color.WHITE };
+
+	static byte[] vertex_strip = { 1, 0, 2, 3 };
+	
+	
+	synchronized static void init()
+	{
+		if (mVertexBuffer != null)
+			return;
+		
+		ByteBuffer vbb = ByteBuffer.allocateDirect(coords.length * 4);
+		vbb.order(ByteOrder.nativeOrder());
+		mVertexBuffer = vbb.asIntBuffer();
+		mVertexBuffer.put(coords);
+		mVertexBuffer.position(0);
+
+		ByteBuffer cbb = ByteBuffer.allocateDirect(colors.length * 4);
+		cbb.order(ByteOrder.BIG_ENDIAN);
+		mColorBuffer = cbb.asIntBuffer();
+		mColorBuffer.put(colors);
+		mColorBuffer.position(0);
+
+		mIndexBuffer = ByteBuffer.allocateDirect(vertex_strip.length);
+		mIndexBuffer.put(vertex_strip);
+		mIndexBuffer.position(0);
+	}
+
 	OpenGLRender render;
-
-	float[] coords = { 20, 10, 20, 0, 0, 0, 0, 10 };
-
-	float[] texturCoords = new float[] { 1f, 1f, 1f, 0.005f, 0.005f, 0.005f,
-			0.005f, 1f };
-
-	float[] vertex_strip = { 1, 0, 2, 3 };
 
 	public OpenGLRenderImage(OpenGLRender r) {
 		render = r;
@@ -21,6 +59,21 @@ public class OpenGLRenderImage extends RenderImage {
 	public void draw(int x, int y, boolean align, int baseFrame, int angle,
 			int function, int remapping, int teamColor) {
 		
+		init();
+		
+		render.gl.glPushMatrix();
+		
+		render.gl.glTranslatex(x, y, 0);
+		
+		render.gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+		render.gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+
+		render.gl.glVertexPointer(2, GL10.GL_FIXED, 0, mVertexBuffer);
+		render.gl.glColorPointer(4, GL10.GL_FIXED, 0, mColorBuffer);
+		render.gl.glDrawElements(GL10.GL_TRIANGLE_STRIP, 4, GL10.GL_UNSIGNED_BYTE,
+				mIndexBuffer);
+		
+		render.gl.glPopMatrix();
 	}
 
 }

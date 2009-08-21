997cb0f094ff0452b57b12331cc789d61e993f5a
korshakov.stepan <korshakov.stepan@7feee052-178f-11de-babb-ddc7aea8d8a1>
2009-08-21 17:45:57 +0000

    Added RenderTile for map tile rendering

---
+++ trunk/android/src/hotheart/starcraft/graphics/render/RenderTile.java
@@ -0,0 +1,5 @@
+package hotheart.starcraft.graphics.render;
+
+public abstract class RenderTile {
+	public abstract void draw(int x, int y);
+}
---
+++ trunk/android/src/hotheart/starcraft/graphics/render/opengl/OpenGLTile.java
@@ -0,0 +1,11 @@
+package hotheart.starcraft.graphics.render.opengl;
+
+import hotheart.starcraft.graphics.render.RenderTile;
+
+public class OpenGLTile extends RenderTile {
+
+	@Override
+	public void draw(int x, int y) {
+	}
+
+}
